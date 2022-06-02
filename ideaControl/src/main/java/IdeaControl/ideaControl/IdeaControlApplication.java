96edc50af382d5314f21fb493c4c967771960731
gfonseca <guilhermejfonseca@hotmail.com>
2022-06-02 18:25:40 -0300

    First Commit

---
+++ ideaControl/src/main/java/IdeaControl/ideaControl/IdeaControlApplication.java
@@ -0,0 +1,13 @@
+package IdeaControl.ideaControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaControlApplication.class, args);
+	}
+
+}
---
+++ ideaControl/src/test/java/IdeaControl/ideaControl/IdeaControlApplicationTests.java
@@ -0,0 +1,13 @@
+package IdeaControl.ideaControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
